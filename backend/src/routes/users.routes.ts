e85efd551aa11597df1f4ebe88a561cd351e9005
Thiago-Santos-SI <thiiagosantos300@gmail.com>
2020-05-29 20:26:45 -0300

    starting to create service-repo-models Users

---
+++ backend/src/routes/users.routes.ts
@@ -0,0 +1,32 @@
+import {Router} from 'express';
+
+import CreateUserService from "../service/CreateUserService";
+
+const usersRouter = Router();
+
+/**
+ * Analise rules in Service
+ */
+
+usersRouter.post('/', async (req, res) =>{
+    try {
+        const {name, email, password} = req.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+
+        return res.json(user)
+
+    } catch(e){
+        return res.status(400).json({error: e.message});
+    }
+});
+
+
+export default usersRouter;
+
---
+++ backend/src/service/CreateUserService.ts
@@ -0,0 +1,41 @@
+import User from "../models/User";
+import {getRepository} from "typeorm";
+
+interface RequestDTO {
+    name: string;
+    email: string;
+    password: string;
+    
+}
+
+/**
+ *  dont need create new repository for User, because just need when the functions personality
+ *  so, use functions of getRepository-typeorm
+ */
+
+class CreateUserService {
+    public async execute({name,email,password}: RequestDTO): Promise<User>{
+        const userRepository = getRepository(User);
+
+        const checkUserExists = await userRepository.findOne({
+            where: {email}
+        });
+
+        if (checkUserExists){
+            throw new Error('Email address already used.')
+        }
+
+        const user = userRepository.create({
+            name,
+            email,
+            password,
+        });
+
+        await userRepository.save(user);
+
+        return user;
+    }
+
+}
+
+export default CreateUserService;
